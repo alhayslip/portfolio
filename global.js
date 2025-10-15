53f02fe54e80fa26711b1db4582929f3f0655257
AHayslip23 <alhayslip@ucsd.edu>
2025-10-14 18:53:41 -0700

    Step 3 Push to Github Redo
    Made a mistake in the previous commit

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-console.log('IT’S ALIVE!');
+console.log("IT’S ALIVE!");
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
@@ -14,28 +14,21 @@ let pages = [
 
 const BASE_PATH =
   location.hostname === 'localhost' || location.hostname === '127.0.0.1'
-    ? '/' // Local server
-    : 'https://github.com/alhayslip/portfolio'; // GitHub Pages repo name
+    ? '/' 
+    : '/portfolio/'; // use your GitHub Pages repo name here
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
+  let url = !p.url.startsWith("http") ? BASE_PATH + p.url : p.url;
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = p.title;
+
+  a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname);
+  a.toggleAttribute("target", a.host !== location.host);
+
+  nav.append(a);
 }
-
- url = !url.startsWith("http") ? BASE_PATH + url : url;
-
-let a = document.createElement('a');
-a.href = url;
-a.textContent = title;
-nav.append(a);
-
-a.classList.toggle(
-  'current',
-  a.host === location.host && a.pathname === location.pathname,
-);
-
-a.toggleAttribute("target", a.host !== location.host);
 
