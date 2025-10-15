3e36360caf16b3408935932557ae18417bcb4819
AHayslip23 <alhayslip@ucsd.edu>
2025-10-14 20:19:13 -0700

    Step 4 Upload
    Upload

--- global.js
+++ global.js
@@ -35,3 +35,33 @@ for (let p of pages) {
 
   nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+  <label class="color-scheme">
+    Theme:
+    <select id="theme-select">
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+`
+);
+
+const select = document.querySelector("#theme-select");
+
+function setColorScheme(scheme) {
+  document.documentElement.style.setProperty("color-scheme", scheme);
+  localStorage.colorScheme = scheme;
+  select.value = scheme;
+}
+
+if ("colorScheme" in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener("input", (event) => {
+  setColorScheme(event.target.value);
+});
