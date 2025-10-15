ab98b4792dd06cd2858739c195348560e5786271
AHayslip23 <alhayslip@ucsd.edu>
2025-10-14 19:24:50 -0700

    Lab 3 Step 3 Redo

--- global.js
+++ global.js
@@ -5,40 +5,31 @@ function $$(selector, context = document) {
 }
 
 let pages = [
-  { url: "", title: "Home Page" },
-  { url: "projects/", title: "Academic Projects" },
-  { url: "contact/", title: "Contact Information" },
-  { url: "https://github.com/alhayslip", title: "My GitHub Page" },
+  { url: "", title: "Home Page"},
+  { url: "projects/", title: "Academic Projects"},
+  { url: "contact/", title: "Contact Information"},
+  { url: "https://github.com/alhayslip", title: "My GitHub Page"},
   { url: "resume/", title: "My Resume"},
 ];
 
-
 const BASE_PATH =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? "/" 
-    : "/portfolio/";
-
+    ? "/"                 
+    : "/portfolio/";       
 
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
-
 for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
-
-  url = !url.startsWith("http") ? BASE_PATH + url : url;
-
-
+  let url = !p.url.startsWith("http") ? BASE_PATH + p.url : p.url;
   let a = document.createElement("a");
   a.href = url;
-  a.textContent = title;
+  a.textContent = p.title;
 
   a.classList.toggle(
     "current",
     a.host === location.host && a.pathname === location.pathname
   );
-
 
   a.toggleAttribute("target", a.host !== location.host);
 
