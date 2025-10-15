41351c555e8c4b1182813b89b6ec3568546f42dc
AHayslip23 <alhayslip@ucsd.edu>
2025-10-14 18:41:34 -0700

    Step 2 Redo for console logs

--- global.js
+++ global.js
@@ -1,17 +1,13 @@
 console.log('IT’S ALIVE!');
 
-const $ = (selector) => document.querySelector(selector);
-const $$ = (selector) => Array.from(document.querySelectorAll(selector));
+let $ = (selector) => document.querySelector(selector);
+let $$ = (selector) => Array.from(document.querySelectorAll(selector));
 
-const navLinks = $$("nav a");
+let navLinks = $$("nav a");
 
 let currentLink = navLinks.find(
   (a) => a.host === location.host && a.pathname === location.pathname,
 );
 
-if (currentLink) {
-  // or if (currentLink !== undefined)
-  currentLink.classList.add('current');
-}
-
 currentLink.classList.add('current');
+
