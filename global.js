5c8cc30bb789ddcef6d89d593f1e5899386d0aab
AHayslip23 <alhayslip@ucsd.edu>
2025-10-14 18:49:21 -0700

    Lab 3 Step 3
    Push Commits

--- global.js
+++ global.js
@@ -1,13 +1,41 @@
 console.log('IT’S ALIVE!');
 
-let $ = (selector) => document.querySelector(selector);
-let $$ = (selector) => Array.from(document.querySelectorAll(selector));
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
 
-let navLinks = $$("nav a");
+let pages = [
+  { url: "", title: "Home Page" },
+  { url: "projects/", title: "Academic Projects" },
+  { url: "contact/", title: "Contact Information" },
+  { url: "resume/", title: "My Resume" },
+  { url: "https://github.com/alhayslip/portfolio", title: "My GitHub Page" },
+];
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/' // Local server
+    : 'https://github.com/alhayslip/portfolio'; // GitHub Pages repo name
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+}
+
+ url = !url.startsWith("http") ? BASE_PATH + url : url;
+
+let a = document.createElement('a');
+a.href = url;
+a.textContent = title;
+nav.append(a);
+
+a.classList.toggle(
+  'current',
+  a.host === location.host && a.pathname === location.pathname,
 );
 
-currentLink.classList.add('current');
+a.toggleAttribute("target", a.host !== location.host);
 
