4fae6826689748678dc64f2e2db3a68e49d961f4
AHayslip23 <alhayslip@ucsd.edu>
2025-10-14 18:19:25 -0700

    Lab 3 Step 2
    Commit for Lab 3 Step 2

--- global.js
+++ global.js
@@ -1,5 +1,17 @@
 console.log('IT’S ALIVE!');
 
-function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => Array.from(document.querySelectorAll(selector));
+
+const navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+if (currentLink) {
+  // or if (currentLink !== undefined)
+  currentLink.classList.add('current');
 }
+
+currentLink.classList.add('current');
