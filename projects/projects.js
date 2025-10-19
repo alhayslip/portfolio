d95c1c6ed82258bbecf4f18b85e5bb1e0146ddcf
AHayslip23 <alhayslip@ucsd.edu>
2025-10-18 18:19:23 -0700

    Fix for Step 1.6 - generate project counter
    Fix for Step 1.6 - generate project counter

--- projects/projects.js
+++ projects/projects.js
@@ -1,23 +1,22 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 async function loadProjects() {
-    try {
-        const projects = await fetchJSON('../lib/projects.json');
-        const projectsContainer = document.querySelector('.projects');
-        const projectsTitle = document.querySelector('projects-title');
-        
-        renderProjects(projects, projectsContainer, 'h2');
+  try {
+    const projects = await fetchJSON('../lib/projects.json');
+    const projectsContainer = document.querySelector('.projects');
+    const projectsTitle = document.querySelector('.projects-title');
 
-        if (projectsTitle){
-            projectsTitle.textContent = `${projects.length} Projects`;
-        }
-    } catch (error){
-        console.error('There is an error associated with loading the projects:', error);
+    renderProjects(projects, projectsContainer, 'h2');
+
+    if (projectsTitle) {
+      projectsTitle.textContent = `${projects.length} Projects`;
     }
+  } catch (error) {
+    console.error('Error loading projects:', error);
+  }
 }
 
 loadProjects();
-
 
 
 
