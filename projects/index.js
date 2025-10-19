bd1a2e30935b07c6aa27f11295fcf67a0049278e
AHayslip23 <alhayslip@ucsd.edu>
2025-10-18 18:42:35 -0700

    Redo for Steps 2.1 and 2.2
    REdo

--- projects/index.js
+++ projects/index.js
@@ -1,9 +1,24 @@
 import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
 
-const projects = await fetchJSON('./lib/projects.json');
-const latestProjects = projects.slice(0, 3);
+async function loadHomeProjects(){
+    try {
+      const projects = await fetchJSON('.lib/projects.json');  
+      const latestProjects = projects.slice(0, 3);
+      const projectsContainer = document.querySelector('projects.');
 
-const projectsContainer = document.querySelector('.projects');
+      if(!projectsContainer){
+        console.error("No .projects exist on this homepage");
+        return ;
+      }
+      renderProjects(latestProjects, projectsContainer, 'h2');
 
-renderProjects(latestProjects, projectsContainer, 'h2');
+    }catch (error) {
+        console.error('There is an error with loading the home projects', error);
+    }
+}
+
+loadHomeProjects();
+
+
+
 
