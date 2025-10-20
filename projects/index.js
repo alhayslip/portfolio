88091d846eeb480db53e6a3dff046d71890ed736
AHayslip23 <alhayslip@ucsd.edu>
2025-10-19 19:45:28 -0700

    Step 3
    Updates for step 3

--- projects/index.js
+++ projects/index.js
@@ -19,6 +19,26 @@ async function loadHomeProjects(){
 
 loadHomeProjects();
 
+import {fetchGitHubData} from './global.js';
+
+const profileStats = document.querySelector('#profile-stats');
+
+async function loadGitHubProfile(){
+  const githubData = await fetchGitHubData('alhayslip');
+}
+
+if (profileStats) {
+  profileStats.innerHTML = `
+        <dl>
+          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+          <dt>Followers:</dt><dd>${githubData.followers}</dd>
+          <dt>Following:</dt><dd>${githubData.following}</dd>
+        </dl>
+    `;
+}
+
+loadGitHubProfile();
 
 
 
