1b4cab09535e0a65c5382d337b5ee5c4ce6d31ae
AHayslip23 <alhayslip@ucsd.edu>
2025-10-19 20:24:52 -0700

    Changes for Step 3 Once Again
    Changes for my github account

--- projects/index.js
+++ projects/index.js
@@ -27,21 +27,27 @@ async function loadGitHubProfile(){
   try{
   const githubData = await fetchGitHubData('alhayslip');
 
-  if (profileStats && githubData) {
-      profileStats.innerHTML += `
-        <dl>
-          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-          <dt>Followers:</dt><dd>${githubData.followers}</dd>
-          <dt>Following:</dt><dd>${githubData.following}</dd>
-        </dl>
-      `;
+   if (!profileStats) {
+      console.error("No element found with ID #profile-stats");
+      return;
     }
-  } catch (err) {
-    console.error("Error loading GitHub profile:", err);
+
+  profileStats.innerHTML += `
+      <dl>
+        <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+        <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+        <dt>Followers:</dt><dd>${githubData.followers}</dd>
+        <dt>Following:</dt><dd>${githubData.following}</dd>
+      </dl>
+    `;
+  } catch (error) {
+    console.error("Error loading GitHub data:", error);
+    if (profileStats) {
+      profileStats.innerHTML += `<p style="color:red;">Failed to load GitHub data.</p>`;
+    }
   }
 }
-
+   
 loadGitHubProfile();
 
 
