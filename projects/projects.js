709fed912f4b326468e08a9bccf2c4c01208e112
AHayslip23 <alhayslip@ucsd.edu>
2025-10-28 21:28:24 -0700

    Lab 5 Step 1 Redo
    Lab 5 Step 1 Redo

--- projects/projects.js
+++ projects/projects.js
@@ -20,22 +20,19 @@ loadProjects();
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-
 const svg = d3.select('#projects-pie-plot');
 
-let data = [1, 2, 3, 4, 5, 5]; 
-let radius = 50;
-
+let data = [1, 2, 3];
 
 let arcGenerator = d3.arc()
   .innerRadius(0)
-  .outerRadius(radius);
+  .outerRadius(50);
 
 let sliceGenerator = d3.pie();
 
 let arcData = sliceGenerator(data);
 
-let arcs = arcData.map((d) => arcGenerator(d));
+let arcs = arcData.map(d => arcGenerator(d));
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
