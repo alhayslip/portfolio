38276390547d273922873fe63b54d91083e74a9c
AHayslip23 <alhayslip@ucsd.edu>
2025-10-28 21:10:56 -0700

    Lab 5 Step 1
    Lab 5 Step 1

--- projects/projects.js
+++ projects/projects.js
@@ -18,6 +18,33 @@ async function loadProjects() {
 
 loadProjects();
 
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+const svg = d3.select('#projects-pie-plot');
+
+let data = [1, 2, 3, 4, 5, 5];
+
+let radius = 50;
+
+
+let arcGenerator = d3.arc()
+  .innerRadius(0) 
+  .outerRadius(radius);
+
+let sliceGenerator = d3.pie();
+
+let arcData = sliceGenerator(data);
+
+let arcs = arcData.map(d => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, i) => {
+  svg.append('path')
+    .attr('d', arc)
+    .attr('fill', colors(i));
+});
+
 
 
 
