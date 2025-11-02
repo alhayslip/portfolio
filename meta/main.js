8e6f32cad633164d403ed849fa84182ac8ae0b2e
AHayslip23 <alhayslip@ucsd.edu>
2025-11-02 10:39:11 -0800

    Lab 6 Step 2.1
    Scatterplot

--- meta/main.js
+++ meta/main.js
@@ -18,7 +18,6 @@ function processCommits(data) {
     .map(([commit, lines]) => {
       const first = lines[0];
       const { author, date, time, timezone, datetime } = first;
-
       const ret = {
         id: commit,
         url: "https://github.com/vis-society/lab-7/commit/" + commit,
@@ -30,61 +29,89 @@ function processCommits(data) {
         hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
         totalLines: lines.length,
       };
-
       Object.defineProperty(ret, "lines", {
         value: lines,
         configurable: true,
         enumerable: false,
         writable: false,
       });
-
       return ret;
     });
 }
 
 function renderCommitInfo(data, commits) {
   const dl = d3.select("#stats").append("dl").attr("class", "stats");
-
   dl.append("dt").html('Total <abbr title="Lines of code">LOC</abbr>');
   dl.append("dd").text(data.length);
-
   dl.append("dt").text("Total commits");
   dl.append("dd").text(commits.length);
+}
 
-  const numFiles = d3.groups(data, (d) => d.file).length;
-  dl.append("dt").text("Number of files");
-  dl.append("dd").text(numFiles);
+function renderScatterPlot(data, commits) {
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 20, right: 40, bottom: 50, left: 60 };
 
-  const maxDepth = d3.max(data, (d) => d.depth);
-  dl.append("dt").text("Maximum depth");
-  dl.append("dd").text(maxDepth);
+  const svg = d3
+    .select("#chart")
+    .append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .style("overflow", "visible");
 
-  const avgDepth = d3.mean(data, (d) => d.depth).toFixed(2);
-  dl.append("dt").text("Average depth");
-  dl.append("dd").text(avgDepth);
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([margin.left, width - margin.right])
+    .nice();
 
-  const fileLengths = d3.rollups(
-    data,
-    (v) => d3.max(v, (v) => v.line),
-    (d) => d.file
-  );
-  const avgFileLength = d3.mean(fileLengths, (d) => d[1]).toFixed(2);
-  dl.append("dt").text("Average file length");
-  dl.append("dd").text(avgFileLength);
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, 24])
+    .range([height - margin.bottom, margin.top]);
 
-  const workByPeriod = d3.rollups(
-    data,
-    (v) => v.length,
-    (d) => new Date(d.datetime).toLocaleString("en", { dayPeriod: "short" })
-  );
-  const mostActivePeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
-  dl.append("dt").text("Most work done");
-  dl.append("dd").text(mostActivePeriod || "Unknown");
+  const dots = svg.append("g").attr("class", "dots");
+
+  dots
+    .selectAll("circle")
+    .data(commits)
+    .join("circle")
+    .attr("cx", (d) => xScale(d.datetime))
+    .attr("cy", (d) => yScale(d.hourFrac))
+    .attr("r", 5)
+    .attr("fill", "steelblue")
+    .attr("opacity", 0.7);
+
+  const xAxis = d3.axisBottom(xScale).ticks(10).tickFormat(d3.timeFormat("%b %d"));
+  const yAxis = d3.axisLeft(yScale).ticks(12).tickFormat((d) => `${d}:00`);
+
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(xAxis)
+    .call((g) => g.append("text")
+      .attr("x", width / 2)
+      .attr("y", 40)
+      .attr("fill", "black")
+      .attr("text-anchor", "middle")
+      .text("Date"));
+
+  svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(yAxis)
+    .call((g) => g.append("text")
+      .attr("x", -40)
+      .attr("y", margin.top)
+      .attr("fill", "black")
+      .attr("text-anchor", "start")
+      .text("Time of day (hours)"));
 }
 
 const data = await loadData();
 const commits = processCommits(data);
+
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
 
 
 
