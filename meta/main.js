6b397371dcabe7c916226243b9c2cdeb67f69d3a
AHayslip23 <alhayslip@ucsd.edu>
2025-11-02 10:25:09 -0800

    Apply Step 1.3
    Apply Commit for Step 1.3

--- meta/main.js
+++ meta/main.js
@@ -1,30 +1,27 @@
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 async function loadData() {
-  const data = await d3.csv('loc.csv', (row) => ({
+  const data = await d3.csv("loc.csv", (row) => ({
     ...row,
     line: +row.line,
     depth: +row.depth,
     length: +row.length,
-    date: new Date(row.date + 'T00:00' + row.timezone),
+    date: new Date(row.date + "T00:00" + row.timezone),
     datetime: new Date(row.datetime),
   }));
   return data;
 }
 
-const data = await loadData();
-
 function processCommits(data) {
   return d3
     .groups(data, (d) => d.commit)
     .map(([commit, lines]) => {
-      let first = lines[0];
-      let { author, date, time, timezone, datetime } = first;
+      const first = lines[0];
+      const { author, date, time, timezone, datetime } = first;
 
-   
-      let ret = {
+      const ret = {
         id: commit,
-        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+        url: "https://github.com/vis-society/lab-7/commit/" + commit,
         author,
         date,
         time,
@@ -34,19 +31,61 @@ function processCommits(data) {
         totalLines: lines.length,
       };
 
-
-      Object.defineProperty(ret, 'lines', {
+      Object.defineProperty(ret, "lines", {
         value: lines,
         configurable: true,
+        enumerable: false,
         writable: false,
-        enumerable: false, 
       });
 
       return ret;
     });
 }
 
+function renderCommitInfo(data, commits) {
+  const dl = d3.select("#stats").append("dl").attr("class", "stats");
+
+  dl.append("dt").html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append("dd").text(data.length);
+
+  dl.append("dt").text("Total commits");
+  dl.append("dd").text(commits.length);
+
+  const numFiles = d3.groups(data, (d) => d.file).length;
+  dl.append("dt").text("Number of files");
+  dl.append("dd").text(numFiles);
+
+  const maxDepth = d3.max(data, (d) => d.depth);
+  dl.append("dt").text("Maximum depth");
+  dl.append("dd").text(maxDepth);
+
+  const avgDepth = d3.mean(data, (d) => d.depth).toFixed(2);
+  dl.append("dt").text("Average depth");
+  dl.append("dd").text(avgDepth);
+
+  const fileLengths = d3.rollups(
+    data,
+    (v) => d3.max(v, (v) => v.line),
+    (d) => d.file
+  );
+  const avgFileLength = d3.mean(fileLengths, (d) => d[1]).toFixed(2);
+  dl.append("dt").text("Average file length");
+  dl.append("dd").text(avgFileLength);
+
+  const workByPeriod = d3.rollups(
+    data,
+    (v) => v.length,
+    (d) => new Date(d.datetime).toLocaleString("en", { dayPeriod: "short" })
+  );
+  const mostActivePeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+  dl.append("dt").text("Most work done");
+  dl.append("dd").text(mostActivePeriod || "Unknown");
+}
+
+const data = await loadData();
 const commits = processCommits(data);
-console.log('Processed commits:', commits);
+renderCommitInfo(data, commits);
+
+
 
 
