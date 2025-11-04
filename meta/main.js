d50309f729bcd8fb158df321750a4806bdcf6c32
AHayslip23 <alhayslip@ucsd.edu>
2025-11-03 20:53:25 -0800

    Update Code for Step 4
    Update Code for Step 4

--- meta/main.js
+++ meta/main.js
@@ -115,6 +115,14 @@ function renderScatterPlot(data, commits) {
     .domain([0, 24])
     .range([usableArea.bottom, usableArea.top]);
 
+  const [minLines, maxLines] = d3.extent(commits, (d) => d.totalLines);
+  const rScale = d3
+    .scaleSqrt()
+    .domain([minLines, maxLines])
+    .range([2, 30]); 
+
+  const sortedCommits = d3.sort(commits, (d) => -d.totalLines);
+
   svg
     .append("g")
     .attr("class", "gridlines")
@@ -126,25 +134,28 @@ function renderScatterPlot(data, commits) {
 
   const dots = svg.append("g").attr("class", "dots");
 
-dots
-  .selectAll("circle")
-  .data(commits)
-  .join("circle")
-  .attr("cx", (d) => xScale(d.datetime))
-  .attr("cy", (d) => yScale(d.hourFrac))
-  .attr("r", 5)
-  .attr("fill", "steelblue")
-  .on("mouseenter", (event, commit) => {
-    renderTooltipContent(commit);
-    updateTooltipVisibility(true);
-    updateTooltipPosition(event);
-  })
-  .on("mousemove", (event) => {
-    updateTooltipPosition(event); // continually update position
-  })
-  .on("mouseleave", () => {
-    updateTooltipVisibility(false);
-  });
+  dots
+    .selectAll("circle")
+    .data(sortedCommits)
+    .join("circle")
+    .attr("cx", (d) => xScale(d.datetime))
+    .attr("cy", (d) => yScale(d.hourFrac))
+    .attr("r", (d) => rScale(d.totalLines))
+    .attr("fill", "steelblue")
+    .style("fill-opacity", 0.7)
+    .on("mouseenter", (event, commit) => {
+      d3.select(event.currentTarget).style("fill-opacity", 1);
+      renderTooltipContent(commit);
+      updateTooltipVisibility(true);
+      updateTooltipPosition(event);
+    })
+    .on("mousemove", (event) => {
+      updateTooltipPosition(event);
+    })
+    .on("mouseleave", (event) => {
+      d3.select(event.currentTarget).style("fill-opacity", 0.7);
+      updateTooltipVisibility(false);
+    });
 
   const xAxis = d3
     .axisBottom(xScale)
